767ec24b1b9c9918f0a58fd4042a9efe0d8c0e81
mkubota1111 <mkubota@mini-1.triad.local>
2016-09-26 12:43:25 -0400

    Creating database

--- app/src/main/java/com/bignerdranch/android/model/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/model/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.model;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +17,8 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -24,6 +29,9 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
         mCrimes = new ArrayList<>();
     }
 
