ae320636813c609bbba0f4df92aeffdc9b52f63e
Marcus Kubota <mvkubota@comcast.net>
2016-09-26 14:12:42 -0400

    end of chapter 14

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -117,4 +117,11 @@ public class CrimeFragment extends Fragment {
     private void updateDate() {
         mDateButton.setText(mCrime.getDate().toString());
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        CrimeLab.get(getActivity())
+                .updateCrime(mCrime);
+    }
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeCursorWrapper.java
@@ -1,10 +1,34 @@
 package com.bignerdranch.android.criminalintent.database;
 
+import android.database.Cursor;
 import android.database.CursorWrapper;
+
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
+import com.bignerdranch.android.model.Crime;
+
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by mke6 on 9/26/2016.
  */
 
 public class CrimeCursorWrapper extends CursorWrapper {
+    public CrimeCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        Crime crime = new Crime(UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+
+        return crime;
+    }
 }
