30b763c1c62d6b2175e7a47b1e55d3562b1e1e1c
Marcus Kubota <mvkubota@comcast.net>
2016-09-26 14:07:05 -0400

    end of chapter 14

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeCursorWrapper.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent.database;
+
+import android.database.CursorWrapper;
+
+/**
+ * Created by mke6 on 9/26/2016.
+ */
+
+public class CrimeCursorWrapper extends CursorWrapper {
+}
