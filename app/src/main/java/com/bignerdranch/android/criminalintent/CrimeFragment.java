aa9376e6ebaaba8f06a656370c30341014e76eec
Marcus Kubota <marcus.kubota@gmail.com>
2016-09-27 14:08:46 -0400

    building

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -118,6 +118,31 @@ public class CrimeFragment extends Fragment {
         mDateButton.setText(mCrime.getDate().toString());
     }
 
+    private String getCrimeReport() {
+        String solvedString = null;
+        if (mCrime.isSolved()) {
+            solvedString = getString(R.string.crime_report_solved);
+        }
+        else {
+            solvedString = getString(R.string.crime_report_unsolved);
+        }
+
+        String dateFormat = "EEE, MMM dd";
+        String dateString = DateFormat.format(dateFormat, mCrime.getDate()).toString();
+
+        String suspect = mCrime.getSuspect();
+        if (suspect == null) {
+            suspect = getString(R.string.crime_report_nosuspect);
+        }
+        else {
+            suspect = getString(R.string.crime_report_suspect, suspect);
+        }
+
+        String report = getString(R.string.crime_report, mCrime.getTitle(), dateString,
+                solvedString, suspect);
+        return report;
+    }
+
     @Override
     public void onPause() {
         super.onPause();
